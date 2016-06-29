7eee1d238128a6bfe10a070243e644b67a60864f
James Gitting <james.gitting@gmail.com>
2016-06-28 22:56:19 -0700

    Test query

--- conn.go
+++ conn.go
@@ -2,14 +2,38 @@ package main
 
 import (
 	"database/sql"
-	"github.com/lib/pq"
+	"fmt"
+	_ "github.com/lib/pq"
+	"log"
+	"os"
 )
 
 func main() {
+	db, err := sql.Open("postgres", fmt.Sprintf("postgres://%s:%s@localhost/dvdrental", os.Getenv("PG_USER"), os.Getenv("PG_PASS")))
 
-	db, err := sql.Open("postgres", "user=james dbname=tiro sslmode=verify-full")
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer db.Close()
 
+	var (
+		id   int
+		name string
+	)
+	rows, err := db.Query("SELECT id, name FROM public.customer_list WHERE id = $1", 1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		err := rows.Scan(&id, &name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println(id, name)
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
